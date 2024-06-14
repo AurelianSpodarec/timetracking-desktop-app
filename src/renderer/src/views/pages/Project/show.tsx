c93cb5bfba5776bdc4a01cadb5d45a76240ee54f
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-06-14 23:59:57 +0100

    feat: added back button

--- src/renderer/src/views/pages/Project/show.tsx
+++ src/renderer/src/views/pages/Project/show.tsx
@@ -1,6 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+
 function ProjectShow() {
+  const navigate = useNavigate();
+
   return (
     <div>
+       <button onClick={() => navigate(-1)}>go back</button>
       ProjectsShow
     </div>
   )
