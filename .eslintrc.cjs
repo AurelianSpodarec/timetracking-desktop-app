745873b2768860000e9f8ff36fe3109515e3f6b0
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-06-14 21:23:23 +0100

    chore: setup some basic eslint rules; needs going over again some in the future

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,28 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  "rules": {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/js/indent": ['error', 1],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "jsx-quotes": [
+      "error",
+      "prefer-double"
+    ]
+  }
 }
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,11 @@
 import Versions from './components/Versions'
 
-function App(): JSX.Element {
+function App() {
   return (
     <>
       <div className="text-6xl text-red-500 bg-green-500">
         HELLO
+
       </div>
       <Versions></Versions>
     </>
